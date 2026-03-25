15b0f3c6654db3a6cd2064f7ffd201321771d842
rudranianjankar-tech <rudranianjankar2005@gmail.com>
2026-03-25 17:52:11 +0000

    loops que

--- learn.java
+++ learn.java
@@ -84,6 +84,7 @@ public class learn {
         }
 
         //7
+        
 
          
 
---
+++ learn2.java
@@ -0,0 +1,74 @@
+import java.util.Scanner;
+public class learn2 {
+    public static void main(String[] args) {
+       /* 7- Scanner sc = new Scanner (System.in);
+        System.out.println("enter a nno. to check");
+        int num = sc.nextInt();
+
+        boolean isPrime = true;
+
+        if(num<=1){
+            isPrime=false;
+        }else{
+            for(int i=2; i<=num/2;i++){
+                if(num%i==0){
+                    isPrime=false;
+                    break;
+                }
+            }
+        }
+        if(isPrime){
+            System.out.println("its prime no.");
+        }else{
+            System.out.println("not prime");
+        }*/
+
+
+            Scanner sc = new Scanner (System.in);
+            System.out.println("enter a no.");
+            int num = sc.nextInt();
+           // int rev=0;
+
+           /* 8-  while(num!=0){
+                int digit = num%10;
+                rev = rev*10+digit;
+                num = num/10;
+            }
+            System.out.println("reverse is"+" " +rev);*/
+
+            /* 9-int original = num;
+            int rev=0;
+
+            while(num!=0){
+               int  digit= num%10;
+                rev = rev*10 + digit;
+                num = num/10;
+            }
+            if(original==rev){
+                System.out.println("pallimdrome");
+            }else{
+                System.out.println("its not");
+            }*/
+
+                int org = num;
+                int sum = 0;
+
+                while(num!=0){
+                    int digit= num%10;
+                    sum = sum+ (digit * digit * digit);
+                    num = num/10;
+
+                }
+
+                if(sum==org){
+                    System.out.println("armstrong");
+                }else{
+                    System.out.println("no");
+                }
+
+
+
+
+    }
+    
+}
